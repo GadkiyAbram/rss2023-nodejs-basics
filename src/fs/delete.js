1abebc14735ffe2415d0adb2a60ce63fcbec2fa1
gadkiyabram <a.s.abramovskiy@tmn3.etagi.com>
2023-06-18 10:04:46 +0500

    Function delete file implemented.

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+const fs = require('fs');
+const path = require("path");
+const {checkIfDirExists} = require("./utils");
+
 const remove = async () => {
+    const fileToRemove = 'fileToRemove.txt';
+    const fileToRemovePath = path.join(__dirname, '/files/', fileToRemove);
+
+    const fileExists = await checkIfDirExists(fileToRemovePath);
+
+    if (!fileExists) {
+        throw new Error('FS Operation failed');
+    }
+
+    await fs.unlinkSync(fileToRemovePath);
     // Write your code here 
 };
 
-await remove();
+remove();
