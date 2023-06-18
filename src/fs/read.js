542ed7fe6326326c03fa8daf774c052a3ada92ff
gadkiyabram <a.s.abramovskiy@tmn3.etagi.com>
2023-06-18 13:49:00 +0500

    Implemented read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const {checkIfDirExists} = require("./utils");
+
 const read = async () => {
+    const fileName = 'fileToRead.txt';
+    const filePath = path.join(__dirname, '/files/', fileName);
+
+    if (!await checkIfDirExists(filePath, fileName)) {
+        throw new Error('FS Operation failed');
+    }
+
+    fs.readFile(filePath, {encoding: 'utf-8'}, (err, fileData) => {
+       if (!err) {
+           console.log(fileData);
+       }
+    });
     // Write your code here 
 };
 
-await read();
+read();
