75c809fa2b5c29c27a5f0f2f0a6b1eb3ffb0ad5d
gadkiyabram <a.s.abramovskiy@tmn3.etagi.com>
2023-06-18 10:09:41 +0500

    Function list file implemented.

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const {checkIfDirExists} = require("./utils");
+
 const list = async () => {
+    const fileFolder = path.join(__dirname, '/files');
+
+    const dirExists = await checkIfDirExists(fileFolder);
+
+    if (!dirExists) {
+        throw new Error('FS Operation failed');
+    }
+
+    fs.readdir(fileFolder, (err, files) => {
+        files.forEach((fileName) => {
+            console.log(fileName);
+        })
+    })
     // Write your code here 
 };
 
-await list();
+list();
